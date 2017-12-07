77ffd63572e506b923d39cf3d34538ea8bd13a41
DimGael <dimgael98@gmail.com>
2017-12-07 20:56:38 +0100

    Création du formulaire se connecter et de la page cible

---
+++ include/pages/seConnecter.inc.php
@@ -0,0 +1,9 @@
+<h1>Se connecter</h1>
+
+<form method="post" cible="seConnecterCible.inc.php">
+
+	<input type="text" name="login" id="loginConnnexion" required="true" autofocus="true" placeholder="Pseudo"><br> <br>
+	<input type="password" name="mdp" id="mdpPassword" required="true" placeholder="Mot de passe"><br> <br>
+
+	<input type="submit" value="Connexion">
+</form>
