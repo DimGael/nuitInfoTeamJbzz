90d53021a8ae31a4db80d4e933e307907931316f
Naivis <petitpepe650@yahoo.fr>
2017-12-07 20:40:35 +0100

    Création de la page de création de compte

---
+++ include/pages/creerCompte.inc.php
@@ -0,0 +1,10 @@
+<h1> Creer un compte </h1>
+
+	Nom : <input type="texte" name="nom" value="nom" required="true" autofocus ="true" mexlenght="30"><br>
+	Prénom : <input type="texte" name="prenom" value="prenom" required="true" mexlenght="30"><br>
+	Pseudo : <input type="texte" name="pseudo" value="pseudo" required="true" mexlenght="30"><br>
+	Mot de passe : <input type="password" name="pwd" id="pwd" required="true" maxlenght=="30"><br>
+	<br>
+
+	<input type="submit" value="Envoyer">
+
