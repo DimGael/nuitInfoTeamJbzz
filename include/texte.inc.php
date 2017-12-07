c0eeecea5000d2057657c298c08cab723bcb294d
DimGael <dimgael98@gmail.com>
2017-12-08 00:47:32 +0100

    Modification du texte.inc.php

--- include/texte.inc.php
+++ include/texte.inc.php
@@ -51,10 +51,7 @@ break;
 case 12 : include_once('pages/seDeconnecter.inc.php');
 break;
 
-case 13 : include_once('pages/creerEvenement.inc.php');
-break;
-
-case 14 : include_once('pages/creerEvenementCible.inc.php');
+case 13 : include_once('pages/creerEvenementCible.inc.php');
 break;
 
 
