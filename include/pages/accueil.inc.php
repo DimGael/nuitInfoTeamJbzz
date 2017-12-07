4db21cfcfecd2dca850ab87745ec281c013c9430
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-07 23:34:46 +0100

    dadda

--- include/pages/accueil.inc.php
+++ include/pages/accueil.inc.php
@@ -5,6 +5,18 @@
 
      <center><h1>Prévention routière</h1></center>
      <br>
+     <?php if(!empty($_SESSION['connexion'])){
+       ?>
+       <div class="col-md-4 col-md-offset-2">
+          <p> Coucou c'est moi lol </p>
+       </div>
+       <div class="col-md-4">
+          <p> Coucou c'est moi lol </p>
+       </div>
+       <?php
+     }
+     else {
+     ?>
 
      <div class="col-md-4 col-md-offset-2">
         <a href="./index.php?page=1"><center><button type="button" class="bouton bouton1">S'inscrire</button></center></a>
@@ -12,6 +24,8 @@
      <div class="col-md-4">
         <a href="./index.php?page=3"><center><button type="button" class="bouton bouton2">Connexion</button></center></a>
      </div>
+   <?php } ?>
+
    </div>
  </div>
 </div>
