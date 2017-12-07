129407431d5174ca88f44b0d18b2ecbad2018508
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-07 21:13:45 +0100

    css

--- include/pages/accueil.inc.php
+++ include/pages/accueil.inc.php
@@ -10,7 +10,7 @@
         <a href="./index.php?page=1"><center><button type="button" class="bouton bouton1">S'inscrire</button></center></a>
      </div>
      <div class="col-md-4">
-       <center><button type="button" class="bouton bouton2">Connexion</button></center>
+        <a href="./index.php?page=1"><center><button type="button" class="bouton bouton2">Connexion</button></center></a>
      </div>
    </div>
  </div>
