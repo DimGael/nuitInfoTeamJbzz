e16e3cb5615a8f29c22088975ad11c5b98ffc396
DimGael <dimgael98@gmail.com>
2017-12-08 00:31:51 +0100

    Réparation du texte.inc

--- include/texte.inc.php
+++ include/texte.inc.php
@@ -51,10 +51,10 @@ break;
 case 12 : include_once('pages/seDeconnecter.inc.php');
 break;
 
-case 13 : include_once('pages/creerEvenement.inc.php')
+case 13 : include_once('pages/creerEvenement.inc.php');
 break;
 
-case 14 : include_once('pages/creerEvenementCible.inc.php')
+case 14 : include_once('pages/creerEvenementCible.inc.php');
 break;
 
 default : 	include_once('pages/accueil.inc.php');
