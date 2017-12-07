352d6ef1865d06249d2f90d74b9926d49d7e677f
DimGael <dimgael98@gmail.com>
2017-12-07 21:17:10 +0100

    Ajout des pages de connexion

--- include/pages/seConnecter.inc.php
+++ include/pages/seConnecter.inc.php
@@ -5,5 +5,13 @@
 	<input type="text" name="login" id="loginConnnexion" required="true" autofocus="true" placeholder="Pseudo"><br> <br>
 	<input type="password" name="mdp" id="mdpPassword" required="true" placeholder="Mot de passe"><br> <br>
 
+	<?php
+		if(isset($_GET['connexion'])){
+			echo "<em>Erreur login ou mot de passe incorrect</em>";
+
+			echo "<br> <br>";
+		}
+	?>
+
 	<input type="submit" value="Connexion">
 </form>
--- include/pages/seConnecterCible.inc.php
+++ include/pages/seConnecterCible.inc.php
@@ -3,5 +3,21 @@
 	//$_POST['login']
 	//$_POST['mdp']
 
-	
+	//Cas réussite de la connexion
+	if(true){
+		?>
+
+		<h1>Connecté</h1>
+		<p>
+			Vous avez été connecté. <br> <br>Redirection en cours ...
+		</p>
+
+		<?php
+		header("Refresh:2; index.php?page=0");
+		exit();
+	}
+	else{
+		header("Location=index.php?page=4&connexion=1");
+		exit();
+	}
 ?>
