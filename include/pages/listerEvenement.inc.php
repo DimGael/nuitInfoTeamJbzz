e51130ce7cab1c2913b7d721d5bde3fbf6478303
ThomasBouriaud <thomas.bouriaud@etu.unilim.fr>
2017-12-08 00:22:52 +0100

    lister evenements onclick js

--- include/pages/listerEvenement.inc.php
+++ include/pages/listerEvenement.inc.php
@@ -13,15 +13,14 @@
 					<tr><th>Titre</th><th>Organisateur</th><th>Date</th><th>Risque</th><th>Lieu</th></tr>
 					<?php
 					foreach ($evenements as $eve){?>
-            <tr><td><?php echo $eve->getTitre();?>
-            </td><td><?php echo $compteManager->getCompteId($eve->getIdUti())->getPseudo();?>
-            </td><td><?php echo $eve->getDate();?>
-            </td><td><?php echo $eve->getRisque();?>
-            </td><td><?php echo $eve->getLieu();?>
-						</td><td><a href="index.php?page=11&id=<?php echo $eve->getIdEve(); ?>"><img class="modifier" src="img/modifier.png" alt="Acceder aux details de l'evenement !" title="modifIcon"/></a></td>
-
+            <tr onclick="window.location='index.php?page=11&id=<?php echo $eve->getIdEve(); ?>';">
+							<td><?php echo $eve->getTitre();?>
+	            </td><td><?php echo $compteManager->getCompteId($eve->getIdUti())->getPseudo();?>
+	            </td><td><?php echo $eve->getDate();?>
+	            </td><td><?php echo $eve->getRisque();?>
+	            </td><td><?php echo $eve->getLieu();?></td>
             </tr>
 					<?php } ?>
 					</table>
 					<br />
-					<p>Cliquez sur le numéro de la personne pour obtenir plus d'informations.</p>
+					<p>Cliquez sur un evenement pour afficher ses details !</p>
