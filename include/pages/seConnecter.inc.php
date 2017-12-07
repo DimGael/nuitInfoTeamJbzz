1f9b1f931dd075bbac0e6127d15612ca99376bd7
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-07 23:25:58 +0100

    dada

--- include/pages/seConnecter.inc.php
+++ include/pages/seConnecter.inc.php
@@ -1,17 +1,20 @@
-<h1>Se connecter</h1>
+
+	<?php
+		if(!empty($_SESSION['connexion'])){
+			echo "<h1>Erreur !</h1>";
+			echo "<p>Vous êtes déjà connecté ! </p>";
+			header("Refresh:2,url='./index.php?page=0'");
+		}
+		else {
+	?>
+	<h1>Se connecter</h1>
 
 <form method="post" action="index.php?page=4">
 
 	<input type="text" name="login" id="loginConnnexion" required="true" autofocus="true" placeholder="Pseudo"><br> <br>
 	<input type="password" name="mdp" id="mdpPassword" required="true" placeholder="Mot de passe"><br> <br>
 
-	<?php
-		if(isset($_GET['connexion'])){
-			echo "<em>Erreur, login ou mot de passe incorrect</em>";
-
-			echo "<br> <br>";
-		}
-	?>
 
 	<input type="submit" value="Connexion">
 </form>
+<?php } ?>
