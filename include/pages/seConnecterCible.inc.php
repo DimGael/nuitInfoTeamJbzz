1b79bba1fd1c943986ec17319ee470efeb2620ab
DimGael <dimgael98@gmail.com>
2017-12-07 22:40:54 +0100

    Implémentation de la connexion

--- include/pages/seConnecterCible.inc.php
+++ include/pages/seConnecterCible.inc.php
@@ -1,25 +1,34 @@
 <?php
 
+	$pdo = new Mydpo();
+	$compteManager = new CompteManager($pdo);
 	//$_POST['login']
 	//$_POST['mdp']
 
 	//Connexion
 
-	//Cas réussite de la connexion
-	if(false){
-		?>
-
-		<h1>Connecté</h1>
-		<p>
-			Vous avez été connecté. <br> <br>Redirection en cours ...
-		</p>
-
-		<?php
-		Header("Refresh:2;index.php?page=0");
-		exit();
+	//On vérifie si le login existe dans la bdd
+	if(!$compteManager->existeDejaPseudo($_POST['login'])){
+		erreurConnexion();
 	}
-	else{
 
+	$compteConnexion = $compteManager->getComptePseudo();
+
+	if($compteConnexion->getMdp() !== $_POST['mdp']){
+		erreurConnexion();
+	}
+	?>
+
+	<h1>Connecté</h1>
+	<p>
+		Vous avez été connecté. <br> <br>Redirection en cours ...
+	</p>
+
+	<?php
+	Header("Refresh:2;index.php?page=0");
+	exit();
+
+	function erreurConnexion(){
 		Header("Refresh:1;index.php?page=3&connexion=1");
 		exit();
 	}
