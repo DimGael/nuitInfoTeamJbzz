951f78d66f9525627426501bc29e1fedb9947d3f
DimGael <dimgael98@gmail.com>
2017-12-07 22:30:31 +0100

    Suppression du var dump

--- classes/CompteManager.class.php
+++ classes/CompteManager.class.php
@@ -8,7 +8,6 @@ class CompteManager{
 	}
 
 	public function add($compte){
-			var_dump($compte);
 			$sql = "INSERT INTO utilisateur (uti_nom, uti_prenom, uti_pseudo, uti_mdp, uti_email)
 					VALUES(:nom, :prenom, :pseudo, :mdp, :email)";
 			$req = $this->db->prepare($sql);
