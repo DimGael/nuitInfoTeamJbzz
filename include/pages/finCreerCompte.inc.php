4c813e1b5b9d75461db8387443e28e49550c4aaf
Naivis <petitpepe650@yahoo.fr>
2017-12-07 20:57:11 +0100

    Création classe Compte

--- include/pages/finCreerCompte.inc.php
+++ include/pages/finCreerCompte.inc.php
@@ -1,5 +1,15 @@
+<?php
+$pdo = new Mypdo();
 
+/*
+$votemanager = new VoteManager($pdo);
 
+$votemanager->add(new Vote(array(
+	'cit_num' => $_SESSION['cit'],
+	'per_num' => $_SESSION['numPersonneConnecte'],
+	'vot_valeur' => $_POST['vote']
+)));*/
+?>
 
 
 <h1> Compte créé ! </h1>
