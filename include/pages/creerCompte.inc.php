d278ae49dfda71038eb896d5c63e5b84735f9f2c
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-08 00:51:26 +0100

    dadaz

--- include/pages/creerCompte.inc.php
+++ include/pages/creerCompte.inc.php
@@ -1,9 +1,9 @@
 <h1> Creer un compte </h1>
 
 <form method="post" action="index.php?page=2">
-	Nom : <input type="texte" name="nom" placeholder="Dupont" required="true" autofocus ="true" mexlenght="30"><br>
-	Prénom : <input type="texte" name="prenom" placeholder="Jean" required="true" mexlenght="30"><br>
-	Pseudo : <input type="texte" name="pseudo" placeholder="jean123" required="true" mexlenght="30"><br>
+	Nom : <input type="texte" name="nom" placeholder="Dupont" required="true" autofocus ="true" maxlenght="30"><br>
+	Prénom : <input type="texte" name="prenom" placeholder="Jean" required="true" maxlenght="30"><br>
+	Pseudo : <input type="texte" name="pseudo" placeholder="jean123" required="true" maxlenght="30"><br>
 	Email : <input type="email" name="mail" id="mail" placeholder="jeandupont@mail.fr" required="true" maxlenght="30"><br>
 	Mot de passe : <input type="password" name="pwd" id="pwd" placeholder="********"  required="true" maxlenght="30"><br>
 	<br>
--- include/pages/seDeconnecter.inc.php
+++ include/pages/seDeconnecter.inc.php
@@ -1,23 +1,25 @@
-<?php
+<div class="container">
+	<div class="row">
+	<?php
+		if(!estConnecte()){
+			?>
+			<center><h1>Erreur !</h1></center>
+			<p>Vous êtes déjà connecté ! </p>
+			<?php
+			header("Refresh:2,url='./index.php?page=0'");
+			exit();
+		}
 
-
-	if(!estConnecte()){
-		?>
-		<h1>Erreur !</h1>
-		<p>Vous êtes déjà connecté ! </p>
-		<?php
-		header("Refresh:2,url='./index.php?page=0'");
+		unset($_SESSION['connexion']);
+	?>
+	<center><h1>Déconnexion</h1></center>
+	<p>
+		Vous avez bien été déconnecté !<br>
+		Redirection en cours ...
+	</p>
+	<?php
+		//header("Refresh:2,url='./index.php?page=0'");
 		exit();
-	}
-
-	unset($_SESSION['connexion']);
-?>
-<h1>Déconnexion</h1>
-<p>
-	Vous avez bien été déconnecté !<br>
-	Redirection en cours ...
-</p>
-<?php
-	header("Refresh:2,url='./index.php?page=0'");
-	exit();
-?>
+	?>
+</div>
+</div>
