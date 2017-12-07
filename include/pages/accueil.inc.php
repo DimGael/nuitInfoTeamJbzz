40acaf3a665a55e8d44418ced144a75fbced3b1a
AlexisNadaud <alexisn87@hotmail.fr>
2017-12-07 20:16:58 +0100

    Ajout

---
+++ include/pages/accueil.inc.php
@@ -0,0 +1,22 @@
+<center><img src="./img/routeBan2.jpg" class="img-responsive"></center>
+<div class="debut">
+<div class="container">
+  <div class="row">
+    <div class="col-xs-12">
+
+     <center><h1>Prévention routière</h1></center>
+     <br>
+
+     <div class="col-md-4">
+       <p> COUCOU </p>
+     </div>
+     <div class="col-md-4">
+       <p> COUCOU </p>
+     </div>
+     <div class="col-md-4">
+       <p> COUCOU </p>
+     </div>
+   </div>
+ </div>
+</div>
+</div>
--- index.php
+++ index.php
@@ -3,30 +3,5 @@
 require_once("include/config.inc.php");
 require_once("include/autoLoad.inc.php");
 require_once("include/header.inc.php");
-?>
-
-<center><img src="./img/routeBan2.jpg" class="img-responsive"></center>
-<div class="debut">
-<div class="container">
-  <div class="row">
-    <div class="col-xs-12">
-
-     <center><h1>Prévention routière</h1></center>
-     <br>
-
-     <div class="col-md-4">
-       <p> COUCOU </p>
-     </div>
-     <div class="col-md-4">
-       <p> COUCOU </p>
-     </div>
-     <div class="col-md-4">
-       <p> COUCOU </p>
-     </div>
-   </div>
- </div>
-</div>
-</div>
-
-<?php
+require_once("include/texte.inc.php");
 require_once("include/footer.inc.php"); ?>
