58cbb26fa43fc9d4056ed76056bf640ec59d55c5
Naivis <petitpepe650@yahoo.fr>
2017-12-08 01:22:24 +0100

    center

--- include/pages/error404.inc.php
+++ include/pages/error404.inc.php
@@ -1,3 +1,3 @@
-<h1>Oh bah non... T'as cassé Titine ! </h1>
+<center><h1>Oh bah non... T'as cassé Titine ! </h1>
 
-<img src="./img/voiture404.jpg" id="voiture404" alt="voiture 404" height="500px" width="500px" />
+<img src="./img/voiture404.jpg" id="voiture404" alt="voiture 404" height="500px" width="500px" /></center>
